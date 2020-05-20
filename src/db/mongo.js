0ae84551a721eb10c60fa3b952e4eec574ee17bc
ABHISHEK AMRUTE <abhishekamrutelfs@gmail.com>
2020-05-20 15:32:49 +0530

    Updated

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://ChatDev:Dots-dev@13@3@cluster0-opdx9.mongodb.net/chat_app_final?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://Abhishek:abhishekamruteonline@cluster0-b9n3j.mongodb.net/test?retryWrites=true&w=majority',{
     useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology: true 
 })
+
+
+
+// mongodb+srv://ChatDev:Dots-dev@13@3@cluster0-opdx9.mongodb.net/chat_app_final?retryWrites=true&w=majority
+
